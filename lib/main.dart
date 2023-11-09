1692bdba53f4a27afab2f4866886956c52368a50
Prateek Thakur <prateekthakur272@gmail.com>
2023-11-10 00:31:26 +0530

    adds basic app

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,21 @@
-void main(){}
+import 'package:flutter/material.dart';
+import 'package:flutter_hidden_menu/screens/home_screen.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const HomeScreen(),
+      theme: ThemeData(
+        useMaterial3: true
+      ),
+    );
+  }
+}
+
+
+void main(){
+  runApp(const App());
+}
